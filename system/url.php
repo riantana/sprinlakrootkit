fb84d75370113aaeee25694273987f95932eff36
riantana <riantana74@gmail.com>
2023-04-02 20:20:12 -0700

    routing

--- system/url.php
+++ system/url.php
@@ -1,2 +1,26 @@
 <?php
 
+$controller='home';
+$method='index';
+$param=[];
+
+
+$dataurl=url();
+
+if (file_exists()) {
+
+}
+
+
+
+
+
+function url(){
+	$url=$_GET['url'];
+	$url=rtrim($url,'/');
+	$url=filter_var($url,FILTER_SANITIZE_URL);
+	$url=explode('/',$url);
+	return $url;
+}
+
+
