eec89a0cec674e0b1af107d1f493f14732b13453
riantana <riantana74@gmail.com>
2023-04-02 20:54:12 -0700

    routing

--- system/url.php
+++ system/url.php
@@ -4,23 +4,38 @@ $controller='home';
 $method='index';
 $param=[];
 
-
 $dataurl=url();
 
-if (file_exists()) {
-
+if (!empty($dataurl)) {
+	if (file_exists('app/controller/'.$dataurl[0].'.php')) {
+		$controller=$dataurl[0];
+		unset($dataurl[0]);
+	}
 }
 
+require_once 'app/controller/'.$controller.'.php';
 
+if (isset($dataurl[1])) {
+	if(function_exists($dataurl[1])){
+		$method=$dataurl[1];
+		unset($dataurl[1]);
+	}
+}
 
+if (!empty($dataurl)) {
+	$param=array_values($dataurl);
+}
 
+call_user_func_array($method,$param);
 
 function url(){
-	$url=$_GET['url'];
-	$url=rtrim($url,'/');
-	$url=filter_var($url,FILTER_SANITIZE_URL);
-	$url=explode('/',$url);
-	return $url;
+	if (isset($_GET['url'])) {
+		$url=$_GET['url'];
+		$url=rtrim($url,'/');
+		$url=filter_var($url,FILTER_SANITIZE_URL);
+		$url=explode('/',$url);
+		return $url;
+	}
 }
 
 
